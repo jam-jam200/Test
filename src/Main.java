7e1d445f4a37214077e5014ed24e90bd88817c32
jam-jam200 <jessieoyaks4j@gmail.com>
2023-10-10 22:07:11 +0100

    new

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.SeleniumTest.ByteStreamsUtils;
+
 public class Main {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
